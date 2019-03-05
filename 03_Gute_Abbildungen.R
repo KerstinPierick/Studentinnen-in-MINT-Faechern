0eb7973ad76f116021193521814a57b0433504b5
Roman Link <romanlink@gmx.de>
2019-03-05 17:20:03 +0100

    Korrigierte NA-Policy und Grafik für Frauenanteil der Studierenden

--- 03_Gute_Abbildungen.R
+++ 03_Gute_Abbildungen.R
@@ -24,18 +24,17 @@ library(htmlwidgets)
 # Graphiksettings laden
 source("R/gg_screen_themes_20190302.R")
 
-# 01 Grafiken für Studienanfänger -------------------------------------------------------
-
-# a) Daten einlesen ---------------------------------------------------------------------
+# 01 Daten einlesen und vorbereiten -----------------------------------------------------
 # Einlesen der Rohdaten
 dat <- read_csv("Daten/tidy/studianfg_zusammfssg.csv", 
                 col_types = list(jahr = col_double()),
-                locale = locale(encoding = "UTF-8")) 
+                locale = locale(encoding = "UTF-8")) %>%
+  mutate(anzahl = ifelse(is.na(anzahl), 0, anzahl)) # NA-Studierendenzahlen (von nicht existenten Fächern) für Summen auf 0 setzen
 
 # Anteile auf Studienbereichslevel
 anteile_sb <- dat %>%
   group_by(fg_code, fg_name, sb_name, studi_typ, geschlecht, jahr, mint) %>%
-  summarise(anzahl = sum(anzahl)) %>%
+  summarise(anzahl = sum(anzahl, na.rm = T)) %>%
   ungroup() %>%
   spread(geschlecht, anzahl) %>%
   mutate(frauenanteil = round(100 * w / (w + m), digits = 1)) %>%
@@ -47,7 +46,7 @@ anteile_sb
 # Anteile auf Fächergruppenlevel
 anteile_fg <- dat %>%
   group_by( fg_name, studi_typ, geschlecht, jahr, fg_code) %>%
-  summarise(anzahl = sum(anzahl)) %>%
+  summarise(anzahl = sum(anzahl, na.rm = T)) %>%
   ungroup() %>%
   spread(geschlecht, anzahl) %>%
   mutate(Frauenanteil = round(100 * w / (w + m), digits = 1)) %>%
@@ -68,7 +67,7 @@ labs <- tibble(fg_name = unique(anteile_fg$fg_name), # nötig um Änderung der F
                  NA)
 )
 
-# Studienanfänger -----------------------------------------------------------------------
+# 02 Plot des Frauenanteils: StudienanfängerInnen ---------------------------------------
 # Erstellen von ggplot2-Objekt
 (p_anfg <- anteile_sb %>%
   rename(Jahr = jahr, Studienbereich = sb_name, Frauenanteil = frauenanteil) %>%
@@ -79,10 +78,10 @@ labs <- tibble(fg_name = unique(anteile_fg$fg_name), # nötig um Änderung der F
   ggplot(aes(x = Jahr)) +
   geom_line(aes(y = Frauenanteil, group = Studienbereich), color = "steelblue", alpha = 0.7) +
   geom_line(data = filter(anteile_fg, studi_typ == "anfaenger"), aes(y = Frauenanteil), lwd = 1.15) + 
-  geom_text(data = labs, aes(x = x, y = y, label = label), size = 3.2) +
+  geom_text(data = labs, aes(x = x, y = y, label = label), size = 2.24) +
   facet_wrap(~fg_name, ncol = 2) +
   geom_hline(aes(yintercept = 50), lty = 2) +
-  labs(title = "Frauenanteil bei den Studienanfängern", x = "Jahr", y = "Frauenanteil (%)\n") +
+  labs(title = "Frauenanteil bei den StudienanfängerInnen", x = "Jahr", y = "Frauenanteil (%)\n") +
   theme_screen() +
   theme(plot.margin = unit(c(3, 3, 3, 10), "mm"))) 
 
@@ -91,37 +90,31 @@ p_anfg1 <- ggplotly(p_anfg, width = 700, height = 1050)
 # Export als html-Widget
 saveWidget(p_anfg1, "p_anfaenger.html")
 
-# Studierende ---------------------------------------------------------------------------
-
-(p_stud <- anteile_sb %>%
+# 03 Plot des Frauenanteils: Studierende ------------------------------------------------
+# Erstellen von ggplot2-Objekt
+(p_studi <- anteile_sb %>%
    rename(Jahr = jahr, Studienbereich = sb_name, Frauenanteil = frauenanteil) %>%
    filter(studi_typ == "studis", fg_code != 10) %>%
    ungroup() %>%
    mutate(fg_name = gsub("ae", "ä", fg_name),
           fg_name = fct_reorder(fg_name, Frauenanteil, mean, na.rm  = T)) %>%
    ggplot(aes(x = Jahr)) +
-   # geom_ribbon(data = anteile_fg, aes(ymin = Frauenanteil - 2*standardfehler * 1000,
-   #                                    ymax = Frauenanteil + 2*standardfehler * 1000),
-   #             fill = "lightgrey", alpha = 0.8) + 
    geom_line(aes(y = Frauenanteil, group = Studienbereich), color = "steelblue", alpha = 0.7) +
    geom_line(data = filter(anteile_fg, studi_typ == "studis"), aes(y = Frauenanteil), lwd = 1.15) + 
+   geom_text(data = labs, aes(x = x, y = y, label = label), size = 2.24) +
    facet_wrap(~fg_name, ncol = 2) +
    geom_hline(aes(yintercept = 50), lty = 2) +
-   labs(title = "Frauenanteil bei den Studierenden", x = "Jahr", y = "Frauenanteil (%)\n", 
-        caption = "Roman Matthias Link und Kerstin Pierick 2019 
-                  Quelle: Statistisches Bundesamt Deutschland") +
+   labs(title = "Frauenanteil bei den Studierenden", x = "Jahr", y = "Frauenanteil (%)\n") +
    theme_screen() +
    theme(plot.margin = unit(c(3, 3, 3, 10), "mm"))) 
 
+# Konvertieren in plotly html-Widget
+p_studi1 <- ggplotly(p_studi, width = 700, height = 1050)
+# Export als html-Widget
+saveWidget(p_studi1, "p_studis.html")
 
-p_stud1 <- ggplotly(p_stud, width = 700, height = 1050) %>%
-  layout(annotations = list(x = 2012, y = 8, font = list(size =8),
-                            text = "Roman Matthias Link und Kerstin Pierick 2019
-Quelle: Statistisches Bundesamt Deutschland", 
-                            showarrow = FALSE))
-saveWidget(p_stud1, "p_studis.html")
 
-# Absolute Zahlen Studi-Anfänger --------------------------------------------------------
+# 04 Absolute Zahlen: Studi-AnfängerInnen -----------------------------------------------
 
 dat %>%
   filter(studi_typ == "anfaenger") %>%
